a70028314ea710998a0e1f6340cfa329cfea82cc
Alexander-Borohov <alexander.borohov17@gmail.com>
2017-05-18 17:20:05 +0300

    Module 14: JPA with Hibernate. Added logout processing

--- module10/jmp-web/src/main/java/com/epam/brest/jmp/web/HomeController.java
+++ module10/jmp-web/src/main/java/com/epam/brest/jmp/web/HomeController.java
@@ -1,7 +1,13 @@
 package com.epam.brest.jmp.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Simple root controller
@@ -15,4 +21,13 @@ public class HomeController {
         return "home";
     }
 
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+            return "redirect:" + "/";
+        }
+        return "redirect:" + "/" + "?logout=false";
+    }
 }
